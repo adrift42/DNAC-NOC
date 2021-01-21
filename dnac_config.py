1cbfbf314a912484cb91a0f2d3d4c3e9d959c3bc
adrift42 <mushrambo28@hotmail.com>
2021-01-22 08:59:58 +1300

    Update dnac_config.py
    Updated config to fix versioning issue

--- dnac_config.py
+++ dnac_config.py
@@ -3,3 +3,4 @@ DNAC= os.getenv("DNAC") or "sandboxdnac2.cisco.com"
 DNAC_USER= os.getenv("DNAC_USER") or "devnetuser"
 DNAC_PORT=os.getenv("DNAC_PORT") or 8080
 DNAC_PASSWORD= os.getenv("DNAC_PASSWORD") or "Cisco123!"
+DNAC_VERSION= os.getenv("DNAC_VERSION") or "1.2.10"
